6e9ad53b22976aa5d7af6c472459b97bb4aa54d3
Md Shazarul Islam <72221964+Shazarulislam@users.noreply.github.com>
2023-05-22 10:44:54 +0600

    Update mux_4_1.v

--- mux_4_1/mux_4_1.v
+++ mux_4_1/mux_4_1.v
@@ -17,12 +17,12 @@ integer i;
 mux_4_1 f(i_0,i_1,i_2,i_3,s_0,s_1,y);
 
 initial begin
-	for(i =0; i<63 ; i=i+1);
+	for(i =0; i<63 ; i=i+1)
 	#10 {i_0,i_1,i_2,i_3,s_0,s_1}=i;
 end
 
 //system task
 initial
-$smonitor ("T=%0t\n s_0=%b s_1=%b i_0=%b i_1=%b i_2=%b i_3=%b y=%b", $realtime,s_0,s_1,i_0,i_1,i_2,i_3,y);
+$monitor ("T=%0t\n s_0=%b s_1=%b i_0=%b i_1=%b i_2=%b i_3=%b y=%b", $realtime,s_0,s_1,i_0,i_1,i_2,i_3,y);
 
 endmodule
